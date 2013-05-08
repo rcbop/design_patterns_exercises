87db37a106b67661512d07bfe4584ad845f05cdd
Rogério Peixoto <rcbop@cesar.org.br>
2013-05-08 14:16:37 -0300

    Adição de novos exercícios

--- DesignPatternsGOF/src/org/cesar/rcbop/p5command/application1/Interruptor.java
+++ DesignPatternsGOF/src/org/cesar/rcbop/p5command/application1/Interruptor.java
@@ -11,11 +11,13 @@ public class Interruptor {
 		this.history = new ArrayList<>();
 	}
 
-
-
 	public void executeAndLogCommand(Command command){
 		command.execute();
 		history.add(command);
+	}
+	
+	public List<Command> getHistory(){
+		return history;
 	}
 
 }
--- DesignPatternsGOF/src/org/cesar/rcbop/p5command/application1/MainTest.java
+++ DesignPatternsGOF/src/org/cesar/rcbop/p5command/application1/MainTest.java
@@ -5,7 +5,33 @@ public class MainTest {
 	public static void main(String[] args) {
 		Interruptor interruptor = new Interruptor();
 		Light roomLight = new Light();
-		interruptor.executeAndLogCommand(new TurnOnCommand(roomLight));
+
+		executeCommands(interruptor, roomLight);
+
+		logCommandHistory(interruptor);
+		
+		
+		testLight(roomLight);
+	}
+
+	private static void testLight(Light roomLight) {
+		System.out.println();
+		roomLight.work();
+	}
+
+	private static void executeCommands(Interruptor interruptor, Light roomLight) {
+		for (int i = 0; i < 5; i++) {
+			interruptor.executeAndLogCommand(new TurnOnCommand(roomLight));
+			interruptor.executeAndLogCommand(new TurnOffCommand(roomLight));
+		}
 		interruptor.executeAndLogCommand(new TurnOffCommand(roomLight));
+		interruptor.executeAndLogCommand(new TurnOnCommand(roomLight));
+	}
+
+	private static void logCommandHistory(Interruptor interruptor) {
+		System.out.println("\n<<<<< COMMAND HISTORY >>>>>\n");
+		for (Command command : interruptor.getHistory()) {
+			System.out.println(command.getClass().getSimpleName());
+		}
 	}
 }
